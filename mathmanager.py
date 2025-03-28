ceb5b43638011e00171d1155ec28453fe6b24aca
Owen2199179 <up2199179@myport.ac.uk>
2025-03-28 10:30:32 +0000

    interest and taxPaid added

--- mathmanager.py
+++ mathmanager.py
@@ -8,3 +8,17 @@ class mathmanager:
 
 	def multiply(self, a, b):
 			return a*b
+	
+	def interest(self, a, b):
+			return a * (1.038^b)
+
+	def taxPaid(self, income):
+		if income < 12570:
+			return 0
+		elif 12570 < income < 50270:
+			return income * 0.8
+		elif 50370 < income < 125140:
+			return income * 0.6
+		else: 
+			return income * 0.55
+
